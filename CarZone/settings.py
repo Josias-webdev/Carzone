045dffe4adaefc2c2545522eebbf1268a9906ec7
Josias-webdev <joejosiasb@gmail.com>
2024-08-06 00:59:54 +0100

    correct setting file error

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -155,9 +155,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'CarZone/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'CarZone/static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
